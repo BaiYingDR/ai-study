04844912be91fc6c86da5de65bd66bbb44f2f935
BaiYing <david-rudai@outlook.com>
2026-01-04 19:47:26 +0800

    predict脚本修改，训练模型添加

--- NLP/input_method/src/predict.py
+++ NLP/input_method/src/predict.py
@@ -15,11 +15,12 @@ def predict(text):
     index2word = {index: word for index, word in enumerate(vocab_list)}
 
     model = InputMethodModule(vocab_size=len(vocab_list)).to(device)
-    model.load_state_dict(torch.load(config.MODELS_DIR / "model.pth"))
+    model.load_state_dict(torch.load(config.MODELS_DIR / "model.pt"))
 
     tokens = jieba.lcut(text)
     indexes = [word2index.get(token, 0) for token in tokens]
     input_tensor = torch.tensor([indexes])
+    input_tensor = input_tensor.to(device)
 
     # model进入evaluate模式
     model.eval()
@@ -29,6 +30,36 @@ def predict(text):
 
     top5_indices = torch.topk(output, 5).indices
 
+    top5_indexes_list = top5_indices.tolist()
+    top5_tokens = [index2word[index] for index in top5_indexes_list[0]]
+
+    return top5_tokens
+
+
+def run_predict():
+    input_history = ""
+    max_history_length = 10000
+    while True:
+        try:
+            input_text = input("请输入：")
+            if input_text == "exit":
+                break
+            if input_text.strip() == "":
+                continue
+            input_history += input_text
+            print(input_history)
+            if len(input_history) > max_history_length:
+                input_history = input_history[-max_history_length:]  # 保留最近的内容
+
+            top5_tokens = predict(input_history)
+            print("top5：", top5_tokens)
+        except KeyboardInterrupt:
+            print("\n程序被用户中断")
+            break
+        except Exception as e:
+            print(f"预测过程中出现错误: {e}")
+            continue
+
 
 if __name__ == '__main__':
-    predict()
+    run_predict()
